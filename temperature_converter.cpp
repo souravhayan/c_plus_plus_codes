3d79f225db1e3da8ebaa19c9a7540aab03e23b79
Sourav Halder <halder23105101502@diu.edu.bd>
2023-09-19 20:28:27 +0600

    Create temperature_converter.cpp

---
+++ temperature_converter.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    float F,C;
+    cout << "Enter fahrenheit to convert into celsius: ";
+    cin >> F;
+
+    C = (F-32)/1.8;
+
+    cout << "In celsius: " << C << endl;
+
+    return 0;
+}
